4ef7d1a18baf7053d53933e65f6f7f564ff04b80
emanuelssonlinnea-rgb <emanuelsson.linnea@gmail.com>
2025-11-19 14:32:53 +0100

    Adding a class

--- io_utils.py
+++ io_utils.py
@@ -28,3 +28,8 @@ def coerce_numeric(df: pd.DataFrame) -> pd.DataFrame: #ensuring data is correctl
 
 df = df.dropna(subset=["id","age","sex","height","weight","systolic_bp","cholesterol","smoker","disease"]).copy()
 
+#Motivering: flyttat över dessa funktioner till egen modul då funktionerna ansvarar för att läsa och klassa datan korrekt. Detta ger bättre struktur på koden och en bättre överblick av kodblocken. 
+# Anledningen till varför jag har en def load_data funktion är för att kunna läsa in "Health_study_dataset"-filen så jag kan manipulera datan i den. 
+# Anledningen till def coerce_numeric funktionen är för för att säkerställa att datan är rätt klassad så att koden jag skriver passar datan. 
+# Den sista raden som börjar med df=... har jag skrivit så att den rensar data-filen, tar bort rader med saknad info, samt skapar om en "ren" version av datafilen som skriver över den gamla.
+# Taget från "Mini-EDA med Python från Joakim"
